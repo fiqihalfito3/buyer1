45d62443919896f008e0ffff05b31ca9c9e29aa3
Fiqih Alfito <fiqihalfito.dev@gmail.com>
2025-06-23 14:35:10 +0700

    init

--- src/commands/default.ts
+++ src/commands/default.ts
@@ -2,5 +2,7 @@ import { reply } from "../services/telegram";
 import type { CommandHandler } from "../types";
 
 export const handleDefault: CommandHandler = async (chatId, env) => {
+    console.log("perintah tidak dikenali");
+
     return reply(chatId, "Perintah tidak dikenali. Ketik /input untuk mulai.", env);
 };
